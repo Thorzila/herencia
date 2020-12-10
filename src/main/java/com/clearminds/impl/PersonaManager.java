afa8cf8ae0c7e4cd691fa60d5c05d2b361b03fad
Juan Manzano <juancamilo106055@gmail.com>
2020-12-10 11:48:08 -0500

    Ejercicio 3 parte final

--- src/main/java/com/clearminds/impl/PersonaManager.java
+++ src/main/java/com/clearminds/impl/PersonaManager.java
@@ -1,14 +1,31 @@
 package com.clearminds.impl;
 
+import java.io.File;
+import java.io.FileReader;
+import java.util.Properties;
+
 import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 
 public class PersonaManager {
 	ServicioPersona srv;
+	
+	public static String leerPropiedad(String leer) {
+		String propiedad = null;
+		try {
+			File file = new File("./config.properties");
+			Properties p = new Properties();
+			p.load(new FileReader(file));
+			propiedad = p.getProperty(leer);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return propiedad;
+	}
 
 	public PersonaManager(){
 		try {
-			Class<?> clase = Class.forName("com.clearminds.interfaces.ServicioPersona");
+			Class<?> clase = Class.forName(leerPropiedad("clase2"));
 			srv = (ServicioPersona)clase.newInstance();			
 		} catch (InstantiationException e) {
 			e.printStackTrace();
